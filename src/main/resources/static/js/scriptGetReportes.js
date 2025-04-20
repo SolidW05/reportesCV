677dabc306c0bd38b6f6d43a928a09052a2ce748
SolidW05 <corona040502@gmail.com>
2025-04-20 10:24:21 -0600

    REcibo de reportes y envios de ellos

---
+++ src/main/resources/static/js/scriptGetReportes.js
@@ -0,0 +1,99 @@
+import { aplicarDataLabels } from "./scripTablas.js";
+
+function obtenerTodosReportes(tablaID) {
+    let url = "http://localhost:7512/api/reporte";
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const tbody = document.querySelector(`#${tablaID} tbody`);
+        tbody.innerHTML = ""; // Limpiar el tbody antes de agregar nuevos datos
+        data.forEach(element => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${element.idReporte}</td>
+                <td>${element.fechaReporte}</td>
+                <td>${element.estado}</td>
+                <td>${element.calle}</td>
+                <td>${element.numero}</td>
+                <td>${element.colonia}</td>
+                <td>${element.municipio}</td>
+                <td>${element.codigoPostal}</td>
+                <td>${element.autoridad}</td>
+                <td>${element.servicio}</td>
+                <td>${element.nombre}</td>
+                <td>${element.telefono}</td>
+            `;
+            tr.id = element.idReporte;
+            tbody.appendChild(tr);
+        });
+        aplicarDataLabels(tablaID);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+}
+function obtenerTodosReportesPorUsr(tablaID) {
+    const idUsr = document.body.dataset.id;
+    let url = `http://localhost:7512/api/reporte/usuarios/${idUsr}`;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const tbody = document.querySelector(`#${tablaID} tbody`);
+        tbody.innerHTML = ""; // Limpiar el tbody antes de agregar nuevos datos
+        data.forEach(element => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${element.idReporte}</td>
+                <td>${element.fechaReporte}</td>
+                <td>${element.estado}</td>
+                <td>${element.calle}</td>
+                <td>${element.numero}</td>
+                <td>${element.colonia}</td>
+                <td>${element.municipio}</td>
+                <td>${element.codigoPostal}</td>
+                <td>${element.autoridad}</td>
+                <td>${element.servicio}</td>
+                <td>${element.nombre}</td>
+                <td>${element.telefono}</td>
+            `;
+            tbody.appendChild(tr);
+        });
+        aplicarDataLabels(tablaID);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+}
+
+window.addEventListener('load', () => {
+    obtenerTodosReportes("tabla-todos")
+    obtenerTodosReportesPorUsr("tabla-usuario")
+
+    setInterval(() => {
+        obtenerTodosReportes("tabla-todos")
+        obtenerTodosReportesPorUsr("tabla-usuario")
+    }, 5000); 
+});
+
---
+++ src/main/resources/static/js/scriptPostReporte.js
@@ -0,0 +1,61 @@
+document.getElementById("formulario-reporte").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const usuario = {
+        idUsuario: document.body.dataset.id,
+        idAutoridad: document.getElementById("autoridad").value,
+        municipio: document.getElementById("municipio").value,
+        colonia: document.getElementById("colonia").value,
+        calle: document.getElementById("calle").value,
+        numero: document.getElementById("numero").value,
+        codigoPostal: document.getElementById("codigoPostal").value,
+        fecha: document.getElementById("fecha").value,
+        descripcion: document.getElementById("descripcion").value,
+    };
+
+    fetch(`http://localhost:7512/api/reporte`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(usuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        showPopup(data); // Mostrar el popup con el id del reporte creado
+    })
+    .catch(error => console.error("Error:", error));
+});
+
+function showPopup(id) {
+    
+    const popup = document.getElementById("popup");
+    const overlay = document.getElementById("overlay");
+    const popupMessage = document.getElementById("popup-message");
+
+    if (id == null) {
+        popupMessage.textContent = "Error al crear el reporte"; // Mensaje personalizado si lo necesitas
+    }
+    else{
+    popupMessage.textContent = `El reporte se ha creado un id ${id}`;
+    }
+    popup.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closePopup() {
+    const popup = document.getElementById("popup");
+    const overlay = document.getElementById("overlay");
+
+    popup.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+document.querySelectorAll("[data-close-button]").forEach(button => {
+    button.addEventListener("click", () => {
+        closePopup();  
+    });
+});
+
+document.getElementById("overlay").addEventListener("click", () => {
+    closePopup();  
+});
+
